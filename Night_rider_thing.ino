9ed26a115d75260861d99dd7de36ae8cf7bc30be
Vita2307 <44173618+Vita2307@users.noreply.github.com>
2018-10-19 12:30:29 -0400

    Add files via upload

---
+++ Night_rider_thing.ino
@@ -0,0 +1,47 @@
+int x=20;
+void setup ()  {
+     pinMode (2, OUTPUT);
+      pinMode (3, OUTPUT); 
+  pinMode (4, OUTPUT);
+  pinMode (5, OUTPUT);
+  pinMode (6, OUTPUT);
+   } // setup
+
+void loop() {
+digitalWrite(2,HIGH);
+delay(x);
+digitalWrite(2,LOW);
+delay(x);
+digitalWrite(3,HIGH);
+delay(x);
+digitalWrite(3,LOW);
+delay(x);
+digitalWrite(4,HIGH);
+delay(x);
+digitalWrite(4,LOW);
+delay(x);
+digitalWrite(5,HIGH);
+delay(x);
+digitalWrite(5,LOW);
+delay(x);
+digitalWrite(6,HIGH);
+delay(x);
+digitalWrite(6,LOW);
+delay(x);
+digitalWrite(5,HIGH);
+delay(x);
+digitalWrite(5,LOW);
+delay(x);
+digitalWrite(4,HIGH);
+delay(x);
+digitalWrite(4,LOW);
+delay(x);
+digitalWrite(3,HIGH);
+delay(x);
+digitalWrite(3,LOW);
+delay(x);
+digitalWrite(2,HIGH);
+delay(x);
+digitalWrite(2,LOW);
+delay(x);
+      } //loop
